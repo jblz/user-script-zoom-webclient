0fc864f40adf14609629421e2b0e52a8672c837d
Jeff Bowen <jblz@users.noreply.github.com>
2022-02-14 16:27:37 -0500

    tweak include pattern include license

--- zoom-redirect-to-web-client.js
+++ zoom-redirect-to-web-client.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        Zoom redirect to web client
 // @namespace   https://github.dev/jblz/user-script-zoom-webclient
-// @include     /https:\/\/(.*\.)?zoom.us\/j\/\d+/
-// @version     1.0.1
+// @include     /^https:\/\/([[a-zA-z\d-]+\.)?zoom.us\/[js]\/\d+/
+// @version     1.0.2
 // @author      jblz
 // @description 2/14/2022, 2:21:23 PM
+// @license     gpl-3.0
 // ==/UserScript==
 
 /* jshint esversion: 6 */
