192276f80322c7ff3f5c6473e86d322d2727a440
Jeff Bowen <jblz@users.noreply.github.com>
2022-02-14 16:12:02 -0500

    tweak redirect line & case insensitve match

--- zoom-redirect-to-web-client.js
+++ zoom-redirect-to-web-client.js
@@ -2,7 +2,7 @@
 // @name        Zoom redirect to web client
 // @namespace   https://github.dev/jblz/user-script-zoom-webclient
 // @include     /https:\/\/(.*\.)?zoom.us\/j\/\d+/
-// @version     1.0.0
+// @version     1.0.1
 // @author      jblz
 // @description 2/14/2022, 2:21:23 PM
 // ==/UserScript==
@@ -11,6 +11,6 @@
 
 (function () {
 	const { origin, pathname, search } = document.location;
-	const [_, meetingID] = pathname.match(/^\/j\/(\d+)/)
-	document.location = `${origin}/wc/join/${meetingID}${search}`;
+	const [_, meetingID] = pathname.match(/^\/j\/(\d+)/i)
+	document.location.pathname = `/wc/join/${ encodeURIComponent( meetingID ) }${search}`;
 })();
