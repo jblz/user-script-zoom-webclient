ff9ffadce5f22b87994556c3a959cfc61326333a
Jeff Bowen <jblz@users.noreply.github.com>
2022-02-14 16:43:25 -0500

    tidy up, error handling

--- zoom-redirect-to-web-client.js
+++ zoom-redirect-to-web-client.js
@@ -2,7 +2,7 @@
 // @name        Zoom redirect to web client
 // @namespace   https://github.dev/jblz/user-script-zoom-webclient
 // @include     /^https:\/\/([[a-zA-z\d-]+\.)?zoom.us\/[js]\/\d+/
-// @version     1.0.2
+// @version     1.0.3
 // @author      jblz
 // @description 2/14/2022, 2:21:23 PM
 // @license     gpl-3.0
@@ -12,6 +12,12 @@
 
 (function () {
 	const { origin, pathname, search } = document.location;
-	const [_, meetingID] = pathname.match(/^\/j\/(\d+)/i)
-	document.location.pathname = `/wc/join/${ encodeURIComponent( meetingID ) }${search}`;
+	const [_, verb, meetingID] = pathname.match( /^\/([js])\/(\d+)/i );
+	if ( ! meetingID ) {
+		return;
+	}
+	document.location = `${ origin }/wc/` +
+		`${ verb === 's' ? 'start' : 'join' }/` +
+		encodeURIComponent( meetingID ) +
+		search || '';
 })();
